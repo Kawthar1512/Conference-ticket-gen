759889b5157cbf95d7fed0a83553bf9ed0872454
Kawthar1512 <kawthar1512@yahoo.com>
2025-02-15 17:48:18 +0100

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom"; // ✅ Correct import
+import { HashRouter } from "react-router-dom"; 
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
